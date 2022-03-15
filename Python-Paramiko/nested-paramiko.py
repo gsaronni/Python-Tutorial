7cf020c92b29b55db7d36849091c6e7b29c69e7b
gsaronni <garloni@protonmail.com>
2022-03-15 18:01:44 +0100

    Added missing variable
    on line 22 of the original file the `sock` variable is missing but in the video you made was there.
    I just added the deleted line from the video.
    I like camelCase.
    
    Thank you very much Vijay Patel for your work.

--- Python-Paramiko/nested-paramiko.py
+++ Python-Paramiko/nested-paramiko.py
@@ -15,15 +15,16 @@ vm.connect(jumphost, username='root', password='a')
 vmtransport = vm.get_transport()
 server_addr = (server, 22) #edited#
 jump_host = (jumphost, 22) #edited#
+vmChannel = neededTransport.open_channel("direct-tcpip", jump_host, server_addr)  # Creates the needed channel to send the connection
 
 jhost = paramiko.SSHClient()
 jhost.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 #jhost.load_host_keys('/home/osmanl/.ssh/known_hosts') #disabled#
-jhost.connect(server, username='root', password='a', sock=vmchannel)
+jhost.connect(server, username='root', password='a', sock=vmChannel)
 
 stdin, stdout, stderr = jhost.exec_command("cat /home/server")
 #
-print stdout.read() #edited#
+print(stdout.read().decode()) # Decode byte to string
 #
 jhost.close()
 vm.close()
